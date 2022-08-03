91e1d50b82e39a3a8e491d7ba6ac04fd3581b3f9
fajarbc <bcfajar@gmail.com>
2022-08-03 13:23:15 +0700

    First commit

---
+++ public/app.js
@@ -0,0 +1,68 @@
+const socket = io("/");
+const peer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+const videos = document.getElementById("video");
+const peers = {};
+const myVideo = createVideo();
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      console.log("ada yang mau masuk");
+      call.answer(stream);
+      const video = createVideo();
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-joined", (userId) => {
+      setTimeout(() => {
+        console.log("user join", userId);
+        userJoinRoom(userId, stream);
+      }, 1000);
+    });
+  });
+
+  socket.on("user-leaved", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+peer.on("open", (id) => {
+  socket.emit("join-room", { roomId: ROOM_ID, userId: id });
+});
+
+function userJoinRoom(userId, stream) {
+  const call = peer.call(userId, stream);
+  const video = createVideo();
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+    socket.emit("leave-room", { roomId: ROOM_ID, userId });
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videos.append(video);
+}
+
+function createVideo() {
+  return document.createElement("video");
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const { v4: randomizer } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/R${randomizer()}`);
+});
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (data) => {
+    console.log("join-room", data);
+    socket.join(data.roomId);
+    socket.to(data.roomId).emit("user-joined", data.userId);
+  });
+  socket.on("leave-room", (data) => {
+    console.log("leave-room", data);
+    socket.to(data.roomId).emit("user-leaved", data.userId);
+  });
+});
+
+server.listen(3000);
